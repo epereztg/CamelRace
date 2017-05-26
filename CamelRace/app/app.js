e51dbb63d3f7ba4c796661fcfae8f35bebc23d84
PAYVISION\e.perez <e.perez@payvision.com>
2017-05-26 09:06:20 +0200

    Improving axis and colors

--- CamelRace/app/app.js
+++ CamelRace/app/app.js
@@ -8,7 +8,7 @@
         // Options
 
         $scope.width = 600;
-        $scope.height = 350;
+        $scope.height = 650;
         $scope.yAxis = 'Ongoing Tasks';
         $scope.xAxis = 'Race!';
 
@@ -68,12 +68,19 @@
         // Find Maximum X & Y Axis Values - this is used to position the data as a percentage of the maximum
         $scope.max = 0;
 
+
+        //$scope.max = Math.max.apply(Math, $scope.data);
         var arrLength = $scope.data.length;
         for (var i = 0; i < arrLength; i++) {
             // Find Maximum X Axis Value
             if ($scope.data[i].value > $scope.max)
                 $scope.max = $scope.data[i].value;
         }
+        //Array.max = function (array) {
+        //    return Math.max.apply(Math, array);
+        //};
+
+        //Console.log($scope.max);
 
         // End Controller  
     });
