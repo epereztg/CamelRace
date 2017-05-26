4a1e6792982da8d6b42529f584e90f7fdd6374a2
PAYVISION\e.perez <e.perez@payvision.com>
2017-05-26 14:43:28 +0200

    Trying dots

--- CamelRace/app/bar-list.component.js
+++ CamelRace/app/bar-list.component.js
@@ -5,7 +5,11 @@ angular.
     module("app").
     component("barList", {   
         template:                    
-        '<div ng-repeat="bar in $ctrl.data" class="bar" style="height:{{$ctrl.width / $ctrl.data.length - 40}}px;width:{{bar.value /  $ctrl.max * $ctrl.height}}px; top:{{$index / $ctrl.data.length * $ctrl.width}}px;"> </div>',
+        '<div ng-repeat="bar in $ctrl.data" class="bar" style="height:{{$ctrl.width / $ctrl.data.length - 40}}px;width:{{bar.total /  $ctrl.max * $ctrl.height}}px; top:{{$index / $ctrl.data.length * $ctrl.width}}px;">' + '<div>&#11044;</div>' + '</div>',
+        //&#11044;
+
+
+        //'<div ng-repeat="bar in $ctrl.data" class="bar" style="height:{{$ctrl.barHeight}}px; width:{{$ctrl.barWidht}}px; top:{{$index / $ctrl.barTop}}px;">{{$index / $ctrl.barWidht}} </div>',
             
         controller: function barListController() {
 
@@ -13,55 +17,56 @@ angular.
             this.height = 650;
             this.yAxis = "Ongoing Tasks";
             this.xAxis = "Race!";
+                      
 
             this.data = [
                 {
-                    label: "January",
-                    value: 36
+                    label: "Task",
+                    total: 36
                 },
                 {
-                    label: "February",
-                    value: 54
+                    label: "Task",
+                    total: 54
                 },
                 {
-                    label: "March",
-                    value: 62
+                    label: "Task",
+                    total: 62
                 },
                 {
-                    label: "April",
-                    value: 82
+                    label: "Task",
+                    total: 82
                 },
                 {
-                    label: "May",
-                    value: 96
+                    label: "Task",
+                    total: 96
                 },
                 {
-                    label: "June",
-                    value: 104
+                    label: "Task",
+                    total: 104
                 },
                 {
-                    label: "July",
-                    value: 122
+                    label: "Task",
+                    total: 122
                 },
                 {
-                    label: "August",
-                    value: 152
+                    label: "Task",
+                    total: 152
                 },
                 {
-                    label: "September",
-                    value: 176
+                    label: "Task",
+                    total: 176
                 },
                 {
-                    label: "October",
-                    value: 180
+                    label: "Task",
+                    total: 180
                 },
                 {
-                    label: "November",
-                    value: 252
+                    label: "Task",
+                    total: 252
                 },
                 {
-                    label: "December",
-                    value: 342
+                    label: "Task",
+                    total: 342
                 }
             ];
 
@@ -73,9 +78,13 @@ angular.
             var arrLength = this.data.length;
             for (var i = 0; i < arrLength; i++) {
                 // Find Maximum X Axis Value
-                if (this.data[i].value > this.max)
-                    this.max = this.data[i].value;
+                if (this.data[i].total > this.max)
+                    this.max = this.data[i].total;                   
             }
+
+            this.barHeight = this.width / this.data.length - 40;
+            this.barWidht = this.data.total / this.max * this.height;
+            this.barTop = this.data.length * this.width;
         }
     });
 
