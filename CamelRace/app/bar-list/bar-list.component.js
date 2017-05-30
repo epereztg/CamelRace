3042e57b030b69971be4129ebde716e6ee378ac5
PAYVISION\e.perez <e.perez@payvision.com>
2017-05-30 16:42:11 +0200

    including json data

--- CamelRace/app/bar-list/bar-list.component.js
+++ CamelRace/app/bar-list/bar-list.component.js
@@ -4,94 +4,26 @@
 angular.
     module("app").
     component("barList", {                 
-        templateUrl: 'app/bar-list/bar-list.template.html',
-        controller: function barListController() {
+        templateUrl: 'app/bar-list/bar-list.template.html',       
+        controller: ['$http', function barListController($http) {
+                var self = this;
+                 $http.get('app/tasks/tasks.json').then(function(response) {               
+                    self.data = response.data;
 
-            this.width = 600;
-            this.height = 600;
-            this.yAxis = "Ongoing Tasks";
-            this.xAxis = "Race!";
-            //Math.round(x)
+                    self.width = 600;
+                    self.height = 600;
+                    self.max = 0;
 
-            this.data = [
-                {
-                    label: "Task",
-                    total: 36,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 54,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 62,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 82,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 96,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 104,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 122,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 152,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 176,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 180,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 252,
-                    current: 10
-                },
-                {
-                    label: "Task",
-                    total: 342,
-                    current: 10
-                }
-            ];
+                    var arrLength = self.data.length;
+                    for (var i = 0; i < arrLength; i++) {
+                        // Find Maximum X Axis Value
+                        if (self.data[i].total > self.max)
+                            self.max = self.data[i].total;
+                    }
 
-            // Find Maximum X & Y Axis Values - this is used to position the data as a percentage of the maximum
-            this.max = 0;
-
-
-            //$scope.max = Math.max.apply(Math, $scope.data);
-            var arrLength = this.data.length;
-            for (var i = 0; i < arrLength; i++) {
-                // Find Maximum X Axis Value
-                if (this.data[i].total > this.max)
-                    this.max = this.data[i].total;
-            }
-
-            this.barHeight = this.width / this.data.length - 40;
-            this.barWidht = this.data.total / this.max * this.height;
-            this.barTop = this.data.length * this.width;
-        }
+                 });                                     
+       
+        }]
     });
 
 
